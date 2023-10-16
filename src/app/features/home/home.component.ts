efbdbff372e8d28b085f1073234e346a201529e7
Guilherme Viana <guilherme.viana@americanas.delivery>
2023-10-16 20:29:13 -0300

    - Adequando e transformando conteúdo da home dinâmica

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Category } from 'src/app/core/models/category.model';
+import { Store } from 'src/app/core/models/store.model';
+import { CategoriesService } from 'src/app/shared/services/categories.service';
+import { StoresService } from 'src/app/shared/services/stores.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +10,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  categories: Category[] = [];
+  stores: Store[] = [];
 
+  constructor(
+    private categoriesService: CategoriesService,
+    private storesService: StoresService,
+  ) { }
+
+  ngOnInit(): void {
+    this.getCategories();
+    this.getStores();
+  }
+
+  getCategories() {
+    this.categoriesService.getCategories().subscribe(
+      response => {
+        this.categories = response;
+      },
+      error => {
+        console.error('Erro ao carregar categorias', error);
+      }
+    )
+  }
+
+  getStores() {
+    this.storesService.getStores().subscribe(
+      response => {
+        this.stores = response;
+      },
+      error => {
+        console.error('Erro ao carregar lojas', error);
+      }
+    )
+  }
 }
