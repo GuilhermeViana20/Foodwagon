4a257f945b82aa3da81ccb3070ddaaf960933d0d
Guilherme Viana <guilherme.viana@americanas.delivery>
2023-10-16 20:28:02 -0300

    - Transformando conteúdo do componente guest em componente passando a ser guest-content

---
+++ src/app/features/guest-content/guest-content.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GuestContentComponent } from './guest-content.component';
+
+describe('GuestContentComponent', () => {
+  let component: GuestContentComponent;
+  let fixture: ComponentFixture<GuestContentComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [GuestContentComponent]
+    });
+    fixture = TestBed.createComponent(GuestContentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/features/guest-content/guest-content.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-guest-content',
+  templateUrl: './guest-content.component.html',
+  styleUrls: ['./guest-content.component.scss']
+})
+export class GuestContentComponent {
+
+}
